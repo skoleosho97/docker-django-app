8efaff230303def428edeb8478b800e0915156d4
waifuxszn <skoleosho97@gmail.com>
2020-09-26 16:51:43 -0400

    Added test case

--- back/core/urls.py
+++ back/core/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from core import settings
-from core.views import HealthCheckView
+from core.views import HealthCheckView, hello_world
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("dj_rest_auth.urls"), name="auth"),
+    path("api/hello_world/", hello_world, name="hello_world"),
     path("api/accounts/", include("accounts.urls"), name="accounts"),
     path("api/site/", include("coursesite.urls"), name="site"),
     path("health_check/", HealthCheckView.as_view(), name="health_check"),
--- front/src/components/Landing.jsx
+++ front/src/components/Landing.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Container from '@material-ui/core/Container';
 import Navigation from "components/Navigation";
 import Grid from '@material-ui/core/Grid';
 
 const Landing = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchHelloWorld = async () => {
+            const url = "/api/hello_world/";
+            const options = {
+                method: "GET",
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            };
+
+            const response = await fetch(url, options);
+
+            // Try catch because .json will throw if no data
+            try {
+            // This will extract the data and convert from a json string to an object
+                const responseData = await response.json();
+                if (response.ok) {
+                    setData(responseData.message);
+                } else {
+                    setError(responseData);
+                }
+            } catch (e) {
+                setError(e);
+            }
+        };
+        fetchHelloWorld();
+    }, []);
+
+    const displayResults = () => (
+        <div>{error ? `Error: ${error}` : `Data: ${data}`}</div>
+    );
+
     return (
         <Container>
             <Navigation />
-            <Grid container>
-                
-            </Grid>
+            {displayResults()}
         </Container>
     );
 };
